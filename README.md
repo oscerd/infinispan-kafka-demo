ce2337c430637fbdfc6d4bb71772242346474d15
Andrea Cosentino <ancosen@gmail.com>
2017-07-27 13:49:58 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Infinispan-Kafka demo
+
+## Introduction
+
+This is an example for Infinispan-kafka connector
+
+## Running
+
+```
+mvn clean package
+export CLASSPATH="$(find target/ -type f -name '*.jar'| grep '\-package' | tr '\n' ':')"
+$KAFKA_HOME/bin/connect-standalone $KAFKA_HOME/config/connect-standalone.properties config/InfinispanSinkConnector.properties
+```
---
+++ src/main/java/org/infinispan/kafka/Author.java
@@ -0,0 +1,26 @@
+package org.infinispan.kafka;
+
+import java.io.Serializable;
+
+import org.infinispan.protostream.annotations.ProtoDoc;
+import org.infinispan.protostream.annotations.ProtoField;
+
+@ProtoDoc("@Indexed")
+public class Author implements Serializable {
+
+   private String name;
+
+   @ProtoField(number = 1, required = true)
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   @Override
+   public String toString() {
+      return "Author [name=" + name + "]";
+   }
+}
